0edbd36aa20d7f0176637865f991716d79073321
Iceskysl <iceskysl@gmail.com>
2013-10-19 22:04:11 +0800

    mo callback

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,17 @@
 module ApplicationHelper
   
+  def notice_message
+    flash_messages = []
+
+    flash.each do |type, message|
+      type = :success if type == :notice
+      text = content_tag(:div, link_to("x", "#", :class => "close","data-dismiss"=>"alert") + message, :class => "alert #{type}")
+      flash_messages << text if message
+    end
+
+    flash_messages.join("\n").html_safe
+  end
+  
   def current_account
     return nil unless session[:account_id]
     @current_account ||= Account.find(session[:account_id])
