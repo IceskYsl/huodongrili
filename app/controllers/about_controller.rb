78c8468666aab96e3b672e662bd36bc9a23cdcd9
Iceskysl <iceskysl@gmail.com>
2013-10-19 22:13:23 +0800

    mo callback

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class AboutController < ApplicationController
   
   def index
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
