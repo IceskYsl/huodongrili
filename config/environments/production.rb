6fcd9d3644e3c2364f4887b5f3e081b4fec04f57
Iceskysl <iceskysl@gmail.com>
2013-10-19 15:55:07 +0800

    add codes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,8 @@ Huodong::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( application.js )
+  config.assets.precompile += %w( application.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
