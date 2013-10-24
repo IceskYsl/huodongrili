c19928528851928b17666be5f97eeac266268a4c
Iceskysl <iceskysl@gmail.com>
2013-10-24 16:19:30 +0800

    fix some code

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,9 +2,9 @@
 class ChatsController < ApplicationController
   
   def index
-    page = params[:page] || 1
-    scoped_items = Chat.normal
-    @chats = scoped_items.recent.paginate(:page => page, :per_page => 100)
+    # page = params[:page] || 1
+    # scoped_items = Chat.normal
+    # @chats = scoped_items.recent.paginate(:page => page, :per_page => 100)
   end
   
   #申请聊天
