2ef700218ae481786e30a7fde5fd9cf92fb52128
Iceskysl <iceskysl@gmail.com>
2013-11-04 08:54:13 +0800

    mo 500

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  # provider :google_oauth2, "480641976417.apps.googleusercontent.com", "xvcihy6jq53tcg5uosPuqze9"
-  provider :google_oauth2, "356696763821.apps.googleusercontent.com", "8cggDS0zZ0MmVTaqsGS_usk6"
-  # provider :google_oauth2, Setting.omniauth_google_oauth2['GOOGLE_KEY'], Setting.omniauth_google_oauth2['GOOGLE_SECRET']  
+  provider :google_oauth2, "480641976417.apps.googleusercontent.com", "xvcihy6jq53tcg5uosPuqze9"
+  # provider :google_oauth2, "356696763821.apps.googleusercontent.com", "8cggDS0zZ0MmVTaqsGS_usk6"
+  #provider :google_oauth2, Setting.omniauth_google_oauth2['GOOGLE_KEY'], Setting.omniauth_google_oauth2['GOOGLE_SECRET']  
 end
